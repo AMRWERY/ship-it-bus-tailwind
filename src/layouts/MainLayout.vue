1c618c8bdf5ce6123367d3f309ed0fa19c3ea34b
Gabor Loth <lgabster@gmail.com>
2023-09-19 15:12:40 +0200

    fix: drawer and auth flow

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -4,9 +4,7 @@
             <div class="px-3 py-3 lg:px-5 lg:pl-3">
                 <div class="flex items-center justify-between">
                     <div class="flex items-center justify-start">
-                        <button data-drawer-target="default-sidebar" data-drawer-toggle="default-sidebar"
-                            aria-controls="default-sidebar" data-drawer-backdrop="true" type="button"
-                            class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
+                        <button aria-controls="default-sidebar" type="button" class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg sm:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" @click="toggleSidebar">
                             <span class="sr-only">Open sidebar</span>
                             <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
                                 xmlns="http://www.w3.org/2000/svg">
@@ -47,7 +45,8 @@
                 </div>
             </div>
         </nav>
-        <aside id="default-sidebar"
+        <aside
+            ref="defaultSidebar"
             class="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0"
             aria-label="Sidebar">
             <div class="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
@@ -133,18 +132,16 @@
 </template>
 
 <script setup>
-import { onMounted, ref, computed } from 'vue';
-import { initDrawers } from 'flowbite';
+import { ref, computed, watchEffect } from 'vue';
+import { Drawer } from 'flowbite';
 import { RouterView } from 'vue-router';
 import { useStore } from 'vuex';
 
 const store = useStore();
-
-onMounted(() => {
-    initDrawers();
-});
-
+let drawer = null;
+let defaultSidebar = ref(null);
 const selectedTab = ref('');
+const isAuthenticated = computed(() => store.getters.isAuthenticated);
 
 const logout = async () => {
     try {
@@ -154,7 +151,17 @@ const logout = async () => {
     }
 }
 
-const isAuthenticated = computed(() => store.getters.isAuthenticated);
+const toggleSidebar = () => {
+    drawer.toggle();
+}
+
+watchEffect(() => {
+    if (defaultSidebar.value && isAuthenticated.value) {
+        drawer = new Drawer(defaultSidebar.value);
+    } else if (defaultSidebar.value && !isAuthenticated.value) {
+        drawer.hide()
+    }
+})
 
 </script>
 
