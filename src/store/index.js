39c0519c2aadccde701ec7407c9719d7222590ce
amr <amrmounir2@gmail.com>
2023-09-27 19:07:20 +0300

    handle some bugs

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,6 @@ import auth from "./modules/auth";
 import products from "./modules/products";
 import categories from "./modules/categories";
 import deal from "./modules/deal";
-import salesCharts from "./modules/salesCharts";
 import invoices from "./modules/invoices";
 import orders from "./modules/orders";
 
@@ -14,7 +13,6 @@ const store = createStore({
     products,
     categories,
     deal,
-    salesCharts,
     invoices,
     orders,
   },
