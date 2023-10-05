0659e3430bf686f4ed22a6ceccf2fec563041030
amr <amrmounir2@gmail.com>
2023-10-05 13:39:00 +0300

    add createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { useStore } from "vuex";
 import Home from "../views/Home.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
